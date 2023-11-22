702cbe83a4731e5d52f045e6b5cb65079837b039
shinobihao2001 <shinobihao2001@gmail.com>
2023-11-22 16:15:52 +0700

    stupid_render

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./src/database");
 const pageService = require("./src/services/pageService.js");
 require("dotenv").config();
+const fs = require("fs");
 connectDB();
 const port = 3000;
 const app = express();
@@ -14,5 +15,8 @@ app.get("*", async (req, res) => {
   const path = (process.env.EN_DOMAIN + req.url).toString();
   console.log(path);
   let html = await pageService.getTranslatePage(path);
-  res.send("Done");
+  //let ren = fs.readFileSync("modified.html", "utf8");
+  res.setHeader("Content-Type", "text/html");
+  console.log(typeof html);
+  res.send(html.toString());
 });
--- src/services/pageService.js
+++ src/services/pageService.js
@@ -3,96 +3,105 @@ const Cheerio = require("cheerio");
 const fs = require("fs");
 const contentService = require("./contentService");
 
-function modifyHTML(url) {
+const crawler = new Crawler({
+  maxConnections: 1,
+});
+
+function modifyHTML(urls, crawler) {
   return new Promise((resolve, reject) => {
-    const crawler = new Crawler({
-      maxConnections: 1,
-      rateLimit: 2000,
-      callback: async (error, res, done) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const newDoc = res.body;
-          const $ = Cheerio.load(newDoc);
-
-          //replace the old link with VNLink
-          $("a").each((index, element) => {
-            const href = $(element).attr("href");
-            if (
-              href &&
-              href.startsWith("https://www.avision.com") &&
-              !href.endsWith("png")
-            ) {
-              $(element).attr(
-                "href",
-                href.replace(
-                  "https://www.avision.com/en/",
-                  "http://localhost:3000/"
-                )
-                // "http://localhost:3000/"
-              );
-            }
-          });
-
-          //replace old text with translate text
-          var doc = $(".elementor-widget-container");
-          for (let index = 0; index < doc.length; index++) {
-            let element = doc.eq(index);
-            let content = element.text().trim();
-
-            if (content) {
-              if (element.children(":not(br)").length == 0) {
-                let newText = await contentService.findTranslatedWord(
-                  element.text().replace(/\s+/g, " ")
-                );
-                element.text(newText);
+    const loop = urls.map((url) => {
+      return new Promise((resolve, reject) => {
+        crawler.queue([
+          {
+            uri: url,
+            callback: async (error, res, done) => {
+              if (error) {
+                console.log(error);
               } else {
-                const children = element.find("*");
-                for (let index = 0; index < children.length; index++) {
-                  let childNode = children.eq(index);
-                  let node = $(childNode);
+                const newDoc = res.body;
+                const $ = Cheerio.load(newDoc);
+
+                //replace the old link with VNLink
+                $("a").each((index, element) => {
+                  const href = $(element).attr("href");
                   if (
-                    node.children(":not(br)").length == 0 &&
-                    node.text().trim()
+                    href &&
+                    href.startsWith("https://www.avision.com") &&
+                    !href.endsWith("png")
                   ) {
-                    let newText = await contentService.findTranslatedWord(
-                      node.text().replace(/\s+/g, " ")
+                    $(element).attr(
+                      "href",
+                      href.replace(
+                        "https://www.avision.com/en",
+                        "http://localhost:3000"
+                      )
+                      // "http://localhost:3000/"
                     );
-                    node.text(newText);
+                  }
+                });
+
+                //replace old text with translate text
+                var doc = $(".elementor-widget-container");
+                for (let index = 0; index < doc.length; index++) {
+                  let element = doc.eq(index);
+                  let content = element.text().trim();
+
+                  if (content) {
+                    if (element.children(":not(br)").length == 0) {
+                      let newText = await contentService.findTranslatedWord(
+                        element.text().replace(/\s+/g, " ")
+                      );
+                      element.text(newText);
+                    } else {
+                      const children = element.find("*");
+                      for (let index = 0; index < children.length; index++) {
+                        let childNode = children.eq(index);
+                        let node = $(childNode);
+                        if (
+                          node.children(":not(br)").length == 0 &&
+                          node.text().trim()
+                        ) {
+                          let newText = await contentService.findTranslatedWord(
+                            node.text().replace(/\s+/g, " ")
+                          );
+                          node.text(newText);
+                        }
+                      }
+                    }
                   }
                 }
+
+                //remove popmade
+                $("#popmake-2659").remove();
+                $("#popmake-11307").remove();
+
+                // Save the modified HTML to a file
+                const modifiedHtml = $.html();
+                fs.writeFileSync("modified.html", modifiedHtml);
+
+                // Open the file in the default web browser
+                // const { exec } = require("child_process");
+                // exec("start modified.html");
+                resolve(modifiedHtml);
               }
-            }
-          }
-
-          //remove popmade
-          $("#popmake-2659").remove();
-          $("#popmake-11307").remove();
-
-          // Save the modified HTML to a file
-          const modifiedHtml = $.html();
-          fs.writeFileSync("modified.html", modifiedHtml);
-
-          // Open the file in the default web browser
-          const { exec } = require("child_process");
-          exec("start modified.html");
-          resolve(modifiedHtml);
-        }
-        done();
-      },
+              done();
+            },
+          },
+        ]);
+      });
     });
-
-    // Queue the URL
-    crawler.queue(url);
+    crawler.once("error", (error) => reject(error));
+    crawler.once("drain", () => {
+      Promise.all(loop).then((results) => {
+        resolve(results);
+      });
+    });
   });
 }
 
 module.exports = {
-  getTranslatePage(url) {
-    modifyHTML(url)
-      .then((html) => {
-        return html;
-      })
-      .catch((error) => console.log(error));
+  async getTranslatePage(url) {
+    let html = await modifyHTML([url], crawler);
+    return html;
   },
 };
