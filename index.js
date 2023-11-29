56c98b7612821c3b7c96390c10da70a013e271af
shinobihao2001 <shinobihao2001@gmail.com>
2023-11-29 08:51:12 +0700

    updateEnv

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 
 connectDB();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
