7e0030fe669feb3bd8c448fee467152799779b3e
shinobihao2001 <shinobihao2001@gmail.com>
2023-12-01 11:21:29 +0700

    fixIconInNewsPage

--- src/services/pageService.js
+++ src/services/pageService.js
@@ -6,59 +6,7 @@ const path = require("path");
 require("dotenv").config();
 require("./overFecth");
 
-// const scriptToInject = `
-//   <script>
-//     // Override the XMLHttpRequest open method
-//     const originalOpen = window.XMLHttpRequest.prototype.open;
-
-//     window.XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
-//       console.log('[XHR Request]', 'Method:', method, 'URL:', url);
-
-//       // Call the original open method
-//       originalOpen.apply(this, arguments);
-
-//       // Override the onreadystatechange method to log responses
-//       const originalOnReadyStateChange = this.onreadystatechange;
-//       this.onreadystatechange = function () {
-//         if (this.readyState === 4) {
-//           //console.log('[XHR Response]', 'Status:', this.status, 'Response:', this.responseText);
-//           console.log("Catch the AJAX")
-//           // Send XHR details and response to the backend
-//           fetch('/api/modify-xhr', {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//               method,
-//               url,
-//               status: this.status,
-//               response: this.responseText,
-//             }),
-//           })
-//           .then(response => response.json())
-//           .then(data => {
-//             console.log("Finish modify AJAX")
-//             //console.log('[Modified XHR Response]', data.modifiedResponse);
-//             setTimeout(()=>{
-//               // Replace the original response with the modified response
-//               this.responseText = data.modifiedResponse;
-
-//               // Call the original onreadystatechange method
-//               if (originalOnReadyStateChange) {
-//                 originalOnReadyStateChange.apply(this, arguments);
-//               }
-//               console.log("Wait 10s done");
-//             },10000) // sleep 10s
-
-//           });
-//         }
-//       };
-//     };
-//   </script>
-// `;
 const fontAwesome = `<script src="https://kit.fontawesome.com/1cbb170ff9.js" crossorigin="anonymous"></script>`;
-//const eicons = `<link rel="stylesheet" href="https://unpkg.com/elementor-icons/style.min.css">`;
 
 const facebookScript = `
     <span class="elementor-grid-item">
@@ -69,7 +17,7 @@ const facebookScript = `
     </span>
 `;
 
-crawler = new Crawler({
+let crawler = new Crawler({
   maxConnections: 1,
 });
 
@@ -86,8 +34,6 @@ function modifyHTML(urls, crawler) {
               } else {
                 const newDoc = res.body;
                 const $$ = Cheerio.load(newDoc);
-                // const $ = res.$;
-                //$("script, style").remove();
 
                 //replace the old link with VNLink and keep the dowload  link
                 //Also the upload link in the CSR page
@@ -152,6 +98,9 @@ function modifyHTML(urls, crawler) {
                     }
                   }
                 }
+                //add font-awsone
+                $$("head").append(fontAwesome);
+
                 //Change title to VN
                 $$("head title").text(function (index, oldText) {
                   return oldText.replace("EN", "VN");
@@ -209,20 +158,9 @@ function modifyHTML(urls, crawler) {
                     '<img style="padding-bottom: 100px; background-repeat: repeat-y" src="https://static3.khuetu.vn/img/m/21.jpg" alt="Your Image Alt Text">'
                   );
                 }
-                //$$(".primary")
 
                 //remove login
                 $$(`a[href="http://localhost:3000/login/"]`).remove();
-
-                //ad override fecth api
-                // const newScript = $$("<script>");
-                // newScript.attr("src", "./overFecth.js");
-                // $$("body").append(newScript);
-                //$$("body").append(scriptToInject);
-                //$$("body").append(scriptToInjectSendingAJAX);
-
-                //add font-awsone
-                $$("head").append(fontAwesome);
 
                 //change eicons to font-awesome
                 // change menu
@@ -241,31 +179,25 @@ function modifyHTML(urls, crawler) {
                   .removeClass("eicon-chevron-left")
                   .addClass("fa fa-chevron-left");
 
+                //change icon user
+                $$("i.icon-user")
+                  .removeClass("icon-user")
+                  .addClass("fa fa-user-o");
+                //Change clock icon
+                $$("i.icon-clock")
+                  .removeClass("icon-clock")
+                  .addClass("fa fa-clock-o");
+                //Change folder icon
+                $$("i.icon-folder")
+                  .removeClass("icon-folder")
+                  .addClass("fa fa-folder-o");
                 //add facebook icon at footer
                 $$("div.elementor-social-icons-wrapper.elementor-grid").append(
                   facebookScript
                 );
-                //add eicons`
-                //$$("head").append(eicons);
 
                 // Save the modified HTML to a file
                 const modifiedHtml = $$.html();
-
-                // fs.writeFileSync(
-                //   url
-                //     .toString()
-                //     .replace(
-                //       "https://www.avision.com/en",
-                //       "http://localhost:3000"
-                //     )
-                //     .replaceAll(":", "_")
-                //     .replaceAll("/", "H"),
-                //   modifiedHtml
-                // );
-
-                // Open the file in the default web browser
-                // const { exec } = require("child_process");
-                // exec("start modified.html");
                 resolve(modifiedHtml);
               }
               done();
@@ -335,65 +267,4 @@ module.exports = {
     }
     return "All page translate done";
   },
-
-  // async modifyAjaxResponse(doc) {
-  //   const $$ = Cheerio.load(doc);
-  //   //replace the old link with VNLink
-  //   $$("a").each((index, element) => {
-  //     const href = $$(element).attr("href");
-  //     if (
-  //       href &&
-  //       href.startsWith("https://www.avision.com") &&
-  //       !href.endsWith("png")
-  //     ) {
-  //       $$(element).attr(
-  //         "href",
-  //         href.replace("https://www.avision.com/en", "http://localhost:3000")
-  //         // "http://localhost:3000/"
-  //       );
-  //     }
-  //   });
-
-  //   //replace old text with translate text
-  //   var doc = $$(process.env.SELECT_TAGS).not("style, script");
-  //   console.log(doc.length);
-  //   for (let index = 0; index < doc.length; index++) {
-  //     console.log(index);
-  //     let element = doc.eq(index);
-  //     let content = element.text().trim();
-
-  //     if (content) {
-  //       // console.log(content);
-  //       if (element.children(":not(br)").length == 0) {
-  //         let newText = await contentService.findTranslatedWord(
-  //           element.text().replace(/\s+/g, " ")
-  //         );
-  //         //console.log(newText);
-  //         $$(element).text(newText);
-  //       } else {
-  //         const children = element.find("*").not("style, script");
-  //         for (let childIndex = 0; childIndex < children.length; childIndex++) {
-  //           let childNode = children.eq(childIndex);
-  //           let node = $$(childNode);
-  //           if (node.children(":not(br)").length == 0 && node.text().trim()) {
-  //             let newText = await contentService.findTranslatedWord(
-  //               node.text().replace(/\s+/g, " ")
-  //             );
-  //             node.text(newText);
-  //             //console.log(newText);
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   //remove compare button
-  //   $$(".compare.button").remove();
-
-  //   // Add a new <p> element
-  //   // $$("li").empty();
-  //   // $$("li").append("<p>Hello, world!</p>");
-  //   //return the modifyResponse
-  //   const modifiedHtml = $$.html();
-  //   return modifiedHtml;
-  // },
 };
--- workflow.js
+++ workflow.js
@@ -36,7 +36,7 @@ require("dotenv").config();
     const links = [
       //"https://www.avision.com/en/shop/mobile-scanner/scanq-sw/",
       //process.env.ORIGIN_URL,
-      "https://www.avision.com/en/product-category/flatbed-scanner/",
+      "https://www.avision.com/en/exhibition/",
     ];
     //let links = await linkService.getLinksNeedToCrawl();
     console.log(links);
