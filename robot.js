56d81dff8361d2ea23933c87774d934629a9c27a
shinobihao2001 <shinobihao2001@gmail.com>
2023-12-07 08:35:53 +0700

    initDemo14_2

--- robot.js
+++ robot.js
@@ -5,9 +5,9 @@ workflow.CrawFunction();
 const timeLapse = 1000 * 60 * 60; // 1 hours
 setInterval(async () => {
   try {
-    console.log("Application start crawling at", new Date().toDateString());
+    console.log("Application start crawling at", new Date());
     await workflow.CrawFunction();
-    console.log("Application finish crawling at", new Date().toDateString());
+    console.log("Application finish crawling at", new Date());
   } catch (error) {
     console.error("Error initializing application:", error);
   }
